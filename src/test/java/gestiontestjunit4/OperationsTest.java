7580f39184836208875613fe03dead63dbfc7def
Mamadou DIOUF <mamadou-d@hotmail.com>
2018-09-18 11:12:23 +0000

    utilisation des log

--- src/test/java/gestiontestjunit4/OperationsTest.java
+++ src/test/java/gestiontestjunit4/OperationsTest.java
@@ -1,7 +1,5 @@
 package gestiontestjunit4;
 
-import java.lang.System.Logger;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -11,39 +9,37 @@ import org.junit.Test;
 
 public class OperationsTest {
 	static Operations op;
-	private static Logger logger;
 	
 	
 	@Before
 	public void init(){
 		
-		logger = null;
-		//		op = new Operations();
-		logger.log(null,"Méthode à lancer avant chaque test");
+//		op = new Operations();
+		 System.out.println("Méthode à lancer avant chaque test");
 	}
 	
 	@After
 	public void testAfter(){
-		logger.log(null,"Méthode à lancer aprés chaque test");
+		System.out.println("Méthode à lancer aprés chaque test");
 	}
 	
 	@AfterClass
 	public static void testAfterClass(){
-		logger.log(null,"Méthode à lancer aprés le test");
+		System.out.println("Méthode à lancer aprés le test");
 	}
 	
 	@BeforeClass
 	public static void creerInstance(){
-		logger.log(null,"Méthode à lancer avant l'execution des tests");
+		System.out.println("Méthode à lancer avant l'execution des tests");
 		op = new Operations();
 	}
 	
 
-	@Test
-	public void testAddition() {
-		long resultat = op.addition(2,5,8);
-		Assert.assertEquals(resultat,2+5+8);
-	}
+	//@Test
+//	public void testAddition() {
+//		long resultat = op.addition(2,5,8);
+//		Assert.assertEquals(resultat,2+5+8);
+//	}
 
 	
 	@Test
